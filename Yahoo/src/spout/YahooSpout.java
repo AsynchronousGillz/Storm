2e81fa4ced7e19a4d97ec7a729752cd093f9ccfc
AsynchronousGillz <ganvana@rams.colostate.edu>
2017-03-08 19:02:24 -0700

    Compile works but running produces an error.

--- Yahoo/src/spout/YahooSpout.java
+++ Yahoo/src/spout/YahooSpout.java
@@ -8,10 +8,15 @@ import org.apache.storm.topology.base.BaseRichSpout;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
 
+//import yahoofinace packages
+import yahoofinance.YahooFinance;
+import yahoofinance.Stock;
+
 import java.util.Map;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 
 public class YahooSpout extends BaseRichSpout implements ISpout {
 	public static final int TOTAL_NUMBERS = 1000;
@@ -21,7 +26,7 @@ public class YahooSpout extends BaseRichSpout implements ISpout {
 	private int index = 0;
 
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		declarer.declare(new Fields("companies"));
+		declarer.declare(new Fields("company", "price"));
 	}
 
 	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
@@ -30,7 +35,12 @@ public class YahooSpout extends BaseRichSpout implements ISpout {
 
 	public void nextTuple() {
 		if(index < TOTAL_NUMBERS) {
-			collector.emit(new Values(companies[index++]));
+			try {
+				String name = companies[index++];
+				BigDecimal price = YahooFinance.get(name).getQuote().getPrice();
+				System.out.println("[ SPOUT INFO ] nextTuple: "+name+" -> "+price);
+				collector.emit(new Values(name, price.doubleValue()));
+			} catch(Exception e) {}
 		}
 	}
 
@@ -50,7 +60,7 @@ public class YahooSpout extends BaseRichSpout implements ISpout {
 		try {
 			scanner = new Scanner(new File("/s/bach/g/under/ganvana/reu/Storm-Projects/Yahoo/companylist.csv"));
 		} catch (FileNotFoundException e) {}
-		for (int i = 0; i < TOTAL_NUMBERS; scanner.hasNextLine()){
+		for (int i = 0; i < TOTAL_NUMBERS && scanner.hasNextLine() == true;){
 			companyList[i++] = scanner.nextLine().split(",")[0];
 		}
 		scanner.close();
